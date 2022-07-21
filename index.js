d969c97e3a613c48fb02ff49f0bcce484607859f
webfaisalbd <web.faisal.bd@gmail.com>
2022-07-21 14:03:32 +0600

    sum of age, avg of age,from array of object , using reduce method

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write my Javascript code!
+const students = [
+  {
+    name: 'John',
+    age: 20,
+  },
+  {
+    name: 'Mohn',
+    age: 30,
+  },
+];
+const totalAge = students.reduce((preV, cur) => {
+  preV = preV + cur.age;
+  return preV;
+}, 0);
+const averageOfAge = totalAge / students.length;
+console.log(totalAge);
+console.log(averageOfAge);
